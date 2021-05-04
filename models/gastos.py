e4e44a7d8db0d236b85aab8d79d624d31abb04d6
Julian Solorzano <yayforme789@gmail.com>
2021-05-04 17:40:01 -0500

    metodo para obtener el resumen por caategorias por fecha

--- models/gastos.py
+++ models/gastos.py
@@ -1,5 +1,5 @@
 import peewee as pw
-from flare import BaseModel
+from flr import BaseModel, db
 
 class CategoriaGasto(BaseModel):
     name = pw.CharField(verbose_name="Nombre")
@@ -12,6 +12,18 @@ class Gasto(BaseModel):
     categoria = pw.ForeignKeyField(CategoriaGasto, verbose_name="Categoría")
     monto = pw.FloatField(verbose_name="Monto")
     chat_id = pw.CharField(verbose_name="Telegram chat id")
+
+    @classmethod
+    def resumen(cls, chat_id, fecha):
+        cr = db.execute_sql("""--begin-sql
+            select c.name, sum(monto)
+            from gasto g
+            join categoria_gasto c on g.categoria_id=c.id
+            where chat_id=%s and fecha::text like %s
+            group by c.name
+        --endsql
+        """, (chat_id, fecha))
+        return [row for row in cr.fetchall()]
 
 Gasto.r()
 
